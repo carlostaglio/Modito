edecdbdbb9cce51c036f62ca2181397f092d964c
carlostaglio <carlostaglio34@gmail.com>
2025-03-14 13:46:58 -0300

    Cambios guardados

--- scripts.js
+++ scripts.js
@@ -26,8 +26,74 @@ const messages = [
     },
     {
         role: 'assistant',
-        content: 'Soy Modito, un chatbot especializado en eficiencia energética, ¿En qué puedo ayudarte?'
-    }
+        content: 'Soy Modito, un chatbot especializado en eficiencia energética creado por Modo Ahorro, ¿En qué puedo ayudarte?'
+    },
+    {
+        role: 'user',
+        content: '¿Cuál es la finalidad de realizar una gestoría energética?'
+    },
+    {
+        role: 'assistant',
+        content: 'La finalidad de hacer una gestoría energética es optimizar el consumo de energía, reducir costos y mejorar la eficiencia energética en una organización o instalación, mediante la planificación, implementación y seguimiento de estrategias y acciones que permitan un uso racional y sostenible de la energía.'
+    },
+    {
+        role: 'user',
+        content: '¿Cuáles son los beneficios que aporta reducir el consumo energético?'
+    },
+    {
+        role: 'assistant',
+        content: `Los beneficios de reducir el consumo energético incluyen:
+- Ahorro económico
+- Reducción de emisiones de gases de efecto invernadero
+- Mejora de la eficiencia energética
+- Disminución de la dependencia de fuentes de energía no renovables
+- Contribución a la sostenibilidad ambiental
+- Mejora de la imagen y la responsabilidad social de la organización.`
+    },
+    {
+        role: 'user',
+        content: '¿Cómo es el estudio, planificación e implementación de una gestoría energética?'
+    },
+    {
+        role: 'assistant',
+        content: `El estudio, planificación e implementación de una gestoría energética implica:
+- Análisis de la situación actual de consumo energético
+- Identificación de oportunidades de mejora
+- Establecimiento de objetivos y metas de eficiencia energética
+- Desarrollo de un plan de acción para reducir el consumo energético
+- Implementación de medidas de eficiencia energética
+- Monitoreo y seguimiento del progreso
+- Evaluación y ajuste continuo del plan para asegurar su efectividad.`
+    },
+    {
+        role: 'user',
+        content: '¿Cuál es el propósito del monitoreo, y cómo se aplica?'
+    },
+    {
+        role: 'assistant',
+        content: `El propósito del monitoreo es recopilar y analizar datos en tiempo real para evaluar el desempeño y la eficiencia de los sistemas y procesos, identificar áreas de mejora y tomar decisiones informadas para optimizar el consumo de energía y reducir costos.
+La aplicación de monitoreo se lleva a cabo a través de la plataforma de Modo Ahorro, que utiliza tecnología de TeraObject para recopilar y analizar datos en tiempo real.
+Además, actualmente estamos trabajando en el desarrollo de una solución de Inteligencia Artificial (IA) para predicciones, lo que permitirá mejorar el desempeño de la plataforma, proporcionar predicciones más precisas y brindar recomendaciones personalizadas para optimizar **el consumo de energía.`
+    },
+    {
+        role: 'user',
+        content: '¿Qué es TeraObject?'
+    },
+    {
+        role: 'assistant',
+        content: `Teraobject es una empresa internacional creada en Argentina en el año 2016 cuyo objetivo es brindar a las industrias servicios de mantenimiento y proyectos de mejora continua. 
+Su principal desarrollo es una plataforma tecnológica propia denominada "Sherlock" (lanzada en el 2019) la cual fue pensada como una herramienta para mejorar los servicios para la industria, mejorando la interoperabilidad de sus procesos productivos y el manejo de información generada por dispositivos IOT.
+`
+    },
+    {
+        role: 'user',
+        content: '¿Qué es ModoAhorro o Modo Ahorro?'
+    },
+    {
+        role: 'assistant',
+        content: `Modo Ahorro es una empresa conformada por un equipo multidisciplinario de expertos en Ingeniería e Informática, cuyo principal objetivo es ayudar a la sociedad a abordar la transición energética. 
+        Su misión es ayudar a hogares y empresas con tecnología 4.0 para cumplir los objetivos de hacer un uso más eficiente de la energía, reducir costos y adoptar energías renovables.`
+    },
 
 ];
 
